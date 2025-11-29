2b6083f388aa475a863350ed508916fcb9124526
humor-404 <bigyamkarmacharya@gmail.com>
2025-11-29 14:36:08 +0545

    current site handler added

--- controller/userController.js
+++ controller/userController.js
@@ -1,3 +1,10 @@
-export function handleCurrentSite(req, res) {
-    return res.status.json("Login sucess");
+import { User } from "../model/user.js";
+
+export async function handleCurrentSite(req, res) {
+    try {
+        const user = await User.findOne({ _id: req.user.id });
+        return res.status(200).json({ id: user._id, name: user.name, email: user.email });
+    } catch (error) {
+        res.status(401).json({ message: error.message });
+    }
 }
