28931fcba333047f9f936fd9485f952156f2ccec
humor-404 <bigyamkarmacharya@gmail.com>
2025-11-29 14:22:11 +0545

    middleware added

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import { handleCurrentSite } from '../controller/userController.js';
+import { authorized } from '../middleware/auth.js';
 
 const router = express.Router();
 
-router.get('current', handleCurrentSite);
+router.get('current', authorized, handleCurrentSite);
 
 export default router;
