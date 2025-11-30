844b147d613263446d9f4023bb882c4940c270a5
humor-404 <bigyamkarmacharya@gmail.com>
2025-11-30 17:18:04 +0545

    comment for readability

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,10 +4,10 @@ import { ensureAuth } from '../middleware/auth.js';
 
 const router = express.Router();
 
-router.post('/tasks', ensureAuth, handleTask);
-router.get('/tasks', ensureAuth, getTask);
-router.get('/task/:id', ensureAuth, getTaskByID);
-router.patch('/task/:id', ensureAuth, updateTask);
-router.delete('/task/:id', ensureAuth, deleteTask);
+router.post('/tasks', ensureAuth, handleTask); //create a task
+router.get('/tasks', ensureAuth, getTask); //view all tasks
+router.get('/task/:id', ensureAuth, getTaskByID); //view task by id
+router.patch('/task/:id', ensureAuth, updateTask); //update a task
+router.delete('/task/:id', ensureAuth, deleteTask); //delete a task
 
 export default router;
