a9d6725e8fcdaa8ac7ad800305064b997251950c
humor-404 <bigyamkarmacharya@gmail.com>
2025-11-30 17:14:30 +0545

    task routes added

--- controller/userController.js
+++ controller/userController.js
@@ -1,10 +1,26 @@
 import { User } from "../model/user.js";
 
-export async function handleCurrentSite(req, res) {
-    try {
-        const user = await User.findOne({ _id: req.user.id });
-        return res.status(200).json({ id: user._id, name: user.name, email: user.email });
-    } catch (error) {
-        res.status(401).json({ message: error.message });
-    }
+// export async function handleCurrentSite(req, res) {
+//     try {
+//         const user = await User.findOne({ _id: req.user.id });
+//         return res.status(200).json({ id: user._id, name: user.name, email: user.email });
+//     } catch (error) {
+//         res.status(401).json({ message: error.message });
+//     }
+// }
+
+export function handleTask(req, res) {
+
+}
+export function getTask(req, res) {
+
+}
+export function getTaskByID(req, res) {
+
+}
+export function updateTask(req, res) {
+
+}
+export function deleteTask(req, res) {
+
 }
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,13 @@
 import express from 'express';
-import { handleCurrentSite } from '../controller/userController.js';
-import { authorized } from '../middleware/auth.js';
+import { handleTask, getTask, getTaskByID, updateTask, deleteTask } from '../controller/userController.js';
+import { ensureAuth } from '../middleware/auth.js';
 
 const router = express.Router();
 
-router.get('/current', authorized, handleCurrentSite);
+router.post('/tasks', ensureAuth, handleTask);
+router.get('/tasks', ensureAuth, getTask);
+router.get('/task/:id', ensureAuth, getTaskByID);
+router.patch('/task/:id', ensureAuth, updateTask);
+router.delete('/task/:id', ensureAuth, deleteTask);
 
 export default router;
