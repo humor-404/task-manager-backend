231468c7f2be60f06321bcd63a09d63ee9f08aa1
humor-404 <bigyamkarmacharya@gmail.com>
2025-12-01 08:55:35 +0545

    update task added

--- controller/userController.js
+++ controller/userController.js
@@ -1,14 +1,4 @@
-import { User } from "../model/user.js";
 import { Task } from "../model/task.js";
-
-// export async function handleCurrentSite(req, res) {
-//     try {
-//         const user = await User.findOne({ _id: req.user.id });
-//         return res.status(200).json({ id: user._id, name: user.name, email: user.email });
-//     } catch (error) {
-//         res.status(401).json({ message: error.message });
-//     }
-// }
 
 export async function handleTask(req, res) {
     try {
@@ -29,6 +19,7 @@ export async function handleTask(req, res) {
         return res.status(411).json({ status: error.message });
     }
 }
+
 export async function getTask(req, res) {
     try {
         const tasks = await Task.find({ userId: req.user.id });
@@ -43,28 +34,47 @@ export async function getTask(req, res) {
         return res.status(411).json({ status: error.message });
     }
 }
+
 export async function getTaskByID(req, res) {
     try {
-        const taskId = req.params.id;
-        if (!taskId) {
-            return res.status(400).json({ message: "provide id of a task" });
-        }
-        const task = await Task.findById(taskId);
+        const task = await Task.findOne({
+            _id: req.params.id,
+            userId: req.user.id
+        });
         if (!task) {
             return res.status(404).json({ message: "task not found" });
         }
+
         return res.status(200).json({ task });
     } catch (error) {
         return res.status(411).json({ status: error.message });
     }
 }
-export function updateTask(req, res) {
-    const taskId = req.params.id;
-    if (!taskId) {
-        return res.status(400).json({ message: "provide id of a task" });
-    }
 
+export async function updateTask(req, res) {
+    try {
+        const { title, description, completed } = req.body;
+        const task = await Task.findOne({
+            _id: req.params.id,
+            userId: req.user.id
+        });
+
+        if (!task) {
+            return res.status(404).json({ message: "Task not found" });
+        }
+
+        if (title !== undefined) task.title = title;
+        if (description !== undefined) task.description = description;
+        if (completed !== undefined) task.completed = completed;
+        
+        const updatedTask = await task.save();
+
+        return res.status(200).json({ updatedTask });
+    } catch (error) {
+        return res.status(411).json({ status: error.message });
+    }
 }
+
 export function deleteTask(req, res) {
 
 }
